8a26ba480dc25aaaa320de6dad70fab04a9ee63e
0xSakone <abdramanesakone4@gmail.com>
2023-02-15 21:47:56 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i = 0;
+	while (message[i] != '\0') {
+		putchar(message[i]);
+		i++;
+	}
+	putchar('\n');
+	return (1);
+}
