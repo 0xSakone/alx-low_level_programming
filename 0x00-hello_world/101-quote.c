723699eb2a98248c9bbcf7f9b2f4e83c44040d82
0xSakone <abdramanesakone4@gmail.com>
2023-02-15 21:50:30 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i = 0;
-	while (message[i] != '\0')
+
+	while (m[i] != '\0')
 	{
-		putchar(message[i]);
+		putchar(m[i]);
 		i++;
 	}
 	putchar('\n');
