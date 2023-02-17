00f37e5c849ae0a7ac92d67d26f82a039fab236c
0xSakone <abdramanesakone4@gmail.com>
2023-02-17 22:46:16 +0000

    Condition

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) {
+	if (n > 0)
+	{
 		printf("%i is positive", n);
-	} else {
+	} else
+	{
 		printf("%i is negative", n);
 	}
 	return (0);
