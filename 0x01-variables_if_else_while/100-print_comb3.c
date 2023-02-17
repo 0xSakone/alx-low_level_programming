d39a65f8ccf62d6fb2d80003c236cd6b25386df6
0xSakone <abdramanesakone4@gmail.com>
2023-02-17 23:50:45 +0000

    print alphabet reverse

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,19 +11,19 @@ int main(void)
 {
 	int i;
 
-    for (i = 0; i < 100; i++)
-    {
-        if (i / 10 < i % 10)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            if (i < 89)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+	for (i = 0; i < 100; i++)
+	{
+		if (i / 10 < i % 10)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			if (i < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
