e7ddb03ed133ebf6b9920607adf37dc50e026ece
0xSakone <abdramanesakone4@gmail.com>
2023-02-18 00:09:24 +0000

    print alphabet reverse

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Display all posible digit of two digits 0, 1, 2
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		if (i < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
