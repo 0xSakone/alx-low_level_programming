8496da7b1a9352aa3a8b2aa8d10a2c11127303e3
0xSakone <abdramanesakone4@gmail.com>
2023-02-21 13:17:17 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower check if caractere is lower and return 1 or 0
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	return ((c >= 97 && c <= 127) ? 1 : 0);
+}
