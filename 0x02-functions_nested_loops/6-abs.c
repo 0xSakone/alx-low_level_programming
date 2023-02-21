bfcd8e60f026c8af0f1a4b2b2b4bb3c6706b5e49
0xSakone <abdramanesakone4@gmail.com>
2023-02-21 23:42:55 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - check if a given is result
+ * The argument should be int
+ *
+ * @n: n value to check to check
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+
+}
