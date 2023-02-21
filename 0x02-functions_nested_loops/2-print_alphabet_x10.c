167c71589e0035194be27a1ad01029f35f7bc5ad
0xSakone <abdramanesakone4@gmail.com>
2023-02-21 12:12:40 +0000

    alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Display alphabet character 10x
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char i, u;
+
+	for (u = 0; u < 10; u++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
