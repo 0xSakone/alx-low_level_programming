e01e02f376262dcbdcf3cc7eb57fc087da788a11
0xSakone <abdramanesakone4@gmail.com>
2023-02-21 14:09:34 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if a given argument is alpha and give result
+ * The argument should be char
+ *
+ * @c: character to check
+ *
+ * Return: 1 if its alpha and 0 else
+ */
+int _isalpha(char c)
+{
+	return (((c > 97 && c < 127) || (c > 64 && c < 91) || (c > 47 && c < 58)) ? 1 : 0);
+
+}
