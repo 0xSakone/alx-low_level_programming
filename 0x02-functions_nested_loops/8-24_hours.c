132e43e119094a5e849f3d5aeb3d58625a3abf4e
0xSakone <abdramanesakone4@gmail.com>
2023-02-22 00:47:44 +0000

    24chrono

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * getFirstDigit - check if a given is result
+ * The argument should be int
+ *
+ * @n: n value to check to check
+ *
+ * Return: print and return first digit
+ */
+int getFirstDigit(int n)
+{
+	int first;
+
+	first = n;
+	for (first = n; first >= 10;)
+	{
+		first = first / 10;
+	}
+	return (first);
+}
+
+/**
+ * jack_bauer - print jackbaur
+ * No argument
+ *
+ * Return: print 24 chrono
+ */
+void jack_bauer(void)
+{
+	int h, min;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			if (h > 10)
+			{
+				_putchar('0' + getFirstdigit(h));
+				_putchar('0' + h % 10);
+			}
+			else
+			{
+				_putchar('0');
+				_putchar('0' + h);
+			}
+			if (min > 10)
+			{
+				_putchar('0' + getFirstdigit(min));
+				_putchar('0' + min % 10);
+			}
+			else
+			{
+				_putchar('0');
+				_putchar('0' + min);
+			}
+		}
+	}
+}
