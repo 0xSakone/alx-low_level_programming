88b1fa1a0cfba325cd68d128f535b44021579871
0xSakone <abdramanesakone4@gmail.com>
2023-02-22 01:42:04 +0000

    100 times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * display_number - print number
+ *
+ * @n: integer
+ *
+ * Return: nothing
+ */
+void display_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		display_number(n / 10);
+		n %= 10;
+	}
+	_putchar(n + '0');
+}
+
+/**
+ * print_times_table - print time tables
+ * No argument
+ *
+ * @x: integer
+ *
+ * Return: print times table
+ */
+void print_times_table(int x)
+{
+	int a, b, n;
+
+	for (a = 0; a < x; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			n = a * b;
+			if (n >= 10)
+			{
+				_putchar(' ');
+				display_number(n);
+				if (b != 9)
+				{
+					_putchar(',');
+				}
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar('0' + n);
+				if (b != 9)
+				{
+					_putchar(',');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
