69db0ea675001a7002d8df377a4281745d1a2f73
0xSakone <abdramanesakone4@gmail.com>
2023-02-22 01:33:09 +0000

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * display_number - print number
+ *
+ * @n: integer
+ *
+ * Return: nothing
+ */
+void display_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		display_number(n / 10);
+		n %= 10;
+	}
+	_putchar(n + '0');
+}
+
+/**
+ * print_to_98 - print n to 98
+ *
+ * @n: integer
+ *
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (int i = n; i <= 98; i++)
+		{
+			display_number(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			} else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+	else
+	{
+		for (int i = n; i >= 98; i--)
+		{
+			display_number(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
