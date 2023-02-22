9f846561ee1191f114737a215b78fb26fa03f0fd
0xSakone <abdramanesakone4@gmail.com>
2023-02-22 02:19:26 +0000

    suite fibinacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * display_number - print number
+ *
+ * @n: integer
+ *
+ * Return: nothing
+ */
+void display_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		display_number(n / 10);
+		n %= 10;
+	}
+	_putchar(n + '0');
+}
+
+/**
+ * main - entry function
+ * No argument
+ *
+ * Return: 0 as success
+ */
+int main(void)
+{
+	int a, n, prev;
+
+	n = 1;
+	prev = 1;
+	while (n < 50)
+	{
+		a = n;
+		n += prev;
+		prev = a;
+		display_number(n);
+		_putchar('\n');
+	}
+	return (0);
+}
