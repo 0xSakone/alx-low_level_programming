8730c515aa8f6b75934dfd9ac071fef99edf21fa
0xSakone <abdramanesakone4@gmail.com>
2023-02-23 18:28:24 +0000

    Is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if a given argument is a digit and give result
+ * The argument should be char
+ *
+ * @d: character to check
+ *
+ * Return: 1 if its digit and 0 else
+ */
+int _isdigit(int d)
+{
+	return ((d >= 48 && d <= 51) ? 1 : 0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char a);
 int _isupper(char c);
+int _isdigit(int c);
 
 #endif
