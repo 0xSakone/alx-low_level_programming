826c7e4a77ec5686746b27da31dba6f884d35cf6
0xSakone <abdramanesakone4@gmail.com>
2023-02-23 19:13:52 +0000

    Print integer from 0 to 14, 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * display_number - print number
- *
- * @n: integer
- *
- * Return: nothing
- */
-void display_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n >= 10)
-	{
-		display_number(n / 10);
-		n %= 10;
-	}
-	_putchar(n + '0');
-}
-
-/**
  * more_numbers - display number from 0 to 14 10 times
  *
  * Return: nothing
@@ -35,7 +13,13 @@ void more_numbers(void)
 	{
 		for (j = 0; i < 15; j++)
 		{
-			display_number(j);
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+			else
+				_putchar(j + '0');
 		}
 		_putchar('\n');
 	}
