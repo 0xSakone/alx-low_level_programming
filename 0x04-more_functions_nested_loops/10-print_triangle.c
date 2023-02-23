554263ffe35b729f222a5f9159cc65005a419f4b
0xSakone <abdramanesakone4@gmail.com>
2023-02-23 23:10:02 +0000

    Draw triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - entry function
+ * @size: size of the triangle
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j, v;
+
+	v = 1;
+	for (i = 0; i < size; i++)
+	{
+		v = (size - i) - 2;
+		for (j = 0; j < size; j++)
+		{
+			if (j > v)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
