521f0faea90182419f40f02aa1eaf4bfd4e22f5d
0xSakone <abdramanesakone4@gmail.com>
2023-02-23 18:37:29 +0000

    Multiply two given integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply to given integer
+ * The argument should be int
+ *
+ * @a: integer a
+ *
+ * @b: integer b
+ *
+ * Return: the multiply of a & b
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
