4968b11e3dac13d0c9a8b15a1d0755d728d254c8
0xSakone <abdramanesakone4@gmail.com>
2023-02-23 19:55:35 +0000

    Print integer line in diagonal n times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - display line n diagonal n times
+ *
+ * @n: integer n
+ *
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
