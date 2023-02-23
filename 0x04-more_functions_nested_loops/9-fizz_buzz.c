5fdb0cb1ad714faaea97280ff9b298401e8ba3b9
0xSakone <abdramanesakone4@gmail.com>
2023-02-23 21:50:28 +0000

    Fizz Buzz interview

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * show - display string function
+ * @s: char array
+ * Return: nothing
+ */
+void show(char s[4])
+{
+	int i;
+
+	for (i = 0; i < 4; i++)
+		_putchar(s[i]);
+}
+
+/**
+ * main - main entry function
+ *
+ *
+ * Return: return always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			show("Fizz");
+			show("Buzz");
+		}
+		else if (i % 3 == 0)
+			show("Fizz");
+		else if (i % 5 == 0)
+			show("Buzz");
+		else
+		{
+			if (i >= 10)
+			{
+				_putchar('0' + i / 10);
+				_putchar('0' + i % 10);
+			}
+			else
+				_putchar('0' + i);
+		}
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
+
