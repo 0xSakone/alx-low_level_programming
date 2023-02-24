dd84381eb3daa86cd37eae65e526d100433474fa
0xSakone <abdramanesakone4@gmail.com>
2023-02-24 01:19:54 +0000

    100 prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 
 /**
- * display_number - print number
- *
- * @n: integer
- *
- * Return: nothing
- */
-void display_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n >= 10)
-	{
-		display_number(n / 10);
-		n %= 10;
-	}
-	_putchar(n + '0');
-}
-
-/**
  * main - main entry function
  *
  * Return: return always 0
@@ -30,7 +8,7 @@ void display_number(int n)
 int main(void)
 {
 	long int number;
-	int i, j, old;
+	int j, old;
 
 	old = 1;
 	number = 612852475143;
@@ -46,6 +24,6 @@ int main(void)
 		if (number == 1)
 			break;
 	}
-	display_number(old);
+	printf("%i\n", old);
 	return (0);
 }
