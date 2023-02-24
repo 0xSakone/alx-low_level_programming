8ab69c803b4e551c43e94b07c6c8a0f63e3f5390
0xSakone <abdramanesakone4@gmail.com>
2023-02-24 01:23:18 +0000

    101 number printer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - print number
+ *
+ * @n: integer
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		display_number(n / 10);
+		n %= 10;
+	}
+	_putchar(n + '0');
+}
