9637a99acd321e269a242906d692e358b83a4bce
0xSakone <abdramanesakone4@gmail.com>
2023-02-24 01:15:25 +0000

    100 prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - main entry function
+ *
+ * Return: return always 0
+ */
+int main(void)
+{
+	long int number;
+	int i, j, old;
+
+	old = 1;
+	number = 612852475143;
+	j = 1;
+	while (number > 0)
+	{
+		while (number % j != 0)
+			j++;
+		number = number / j;
+		if (j > old)
+			old = j;
+		j++;
+		if (number == 1)
+			break;
+	}
+	printf("%i\n", old);
+	return (0);
+}
