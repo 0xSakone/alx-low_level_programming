86bae6f454df97f7b8b6b845b25c477b25e5d3bb
0xSakone <abdramanesakone4@gmail.com>
2023-02-27 08:38:56 +0000

    Give the length of variable

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - Return length of given string
+ * @s: string type
+ * Return: lenght of s
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
