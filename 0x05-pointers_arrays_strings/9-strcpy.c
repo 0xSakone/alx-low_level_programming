4984461a02be485cc76e7abc13c6ad422b2fbbb9
0xSakone <abdramanesakone4@gmail.com>
2023-02-27 18:40:26 +0000

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - print last n of half
+ * @s1: given string
+ * @s2: given string 2
+ * Return: copied
+ */
+char *_strcpy(char *s1, char *s2)
+{
+	return (strcpy(s1, s2));
+}
