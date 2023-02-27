50eac45ec298c7dea8be9afba755e2bb6ded091f
0xSakone <abdramanesakone4@gmail.com>
2023-02-27 17:46:18 +0000

    Print odd index

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - print text odd index
+ * @s: given string
+ * Return: nothing
+ */
+void puts2(char *s)
+{
+	int i;
+
+	for (i = 0; i <= (int)strlen(s); i++)
+	{
+		if (i % 2 == 0 || i == 0)
+			_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
