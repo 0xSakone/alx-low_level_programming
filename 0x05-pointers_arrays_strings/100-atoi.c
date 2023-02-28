b90b108c2ad072cdef155dd24f5c27096e1481d6
0xSakone <abdramanesakone4@gmail.com>
2023-02-28 23:25:36 +0000

    convert string to number

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 
 	for (i = 0; i < (int)strlen(s) - 1; i++)
 		if (s[i] == '-')
-			sign = sign*-1;
+			sign = sign * -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 			break;
 	return ((atoi(&s[i]) * (pow(10, 1) / 10)) * sign);
