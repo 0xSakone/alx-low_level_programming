06fcc82e304047b4c4cf6dd93e0d578238fe3086
0xSakone <abdramanesakone4@gmail.com>
2023-03-03 01:54:32 +0000

    3 strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - Compare two string
+ * @s1: string 1
+ * @s2: sring to append
+ * Return: result integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
