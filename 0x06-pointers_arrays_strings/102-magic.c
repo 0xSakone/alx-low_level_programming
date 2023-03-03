ffd464b7891c3638cf1ec2c7bb6f0fc69e12ab22
0xSakone <abdramanesakone4@gmail.com>
2023-03-03 12:33:50 +0000

    memory over write

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int n;
@@ -16,7 +21,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	*(&n + 4) = 98;
+	*(p + 4) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
