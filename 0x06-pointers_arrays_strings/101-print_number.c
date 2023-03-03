7f35a09fba8a8c566d054efc69c7a318c6035e57
0xSakone <abdramanesakone4@gmail.com>
2023-03-03 12:07:17 +0000

    101 number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,12 +6,16 @@
  */
 void print_number(int n)
 {
+	int n2;
+
 	if (n < 0)
 	{
-		n *= -1;
+		n2 = -1 * n;
 		_putchar('-');
 	}
-	if ((n % 10) != n)
-		print_number(n / 10);
-		_putchar((n % 10) + '0');
+	else
+		n2 = n;
+	if ((n2 % 10) != n2)
+		print_number(n2 / 10);
+		_putchar((n2 % 10) + '0');
 }
