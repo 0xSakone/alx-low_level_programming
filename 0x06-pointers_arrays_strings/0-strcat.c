eb9fae5666a6d4b6431a2715cf78632290d8f63d
0xSakone <abdramanesakone4@gmail.com>
2023-03-03 01:39:44 +0000

    0 strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - Append string to other
+ * @dest: string destinatin
+ * @src: sring to append
+ * Return: append strong pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	return strcat(dest, src);
+}
