262eca305fff16c0ad400e2650705301be04be95
0xSakone <abdramanesakone4@gmail.com>
2023-03-03 02:52:35 +0000

    7 string to leet text

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include "main.h"
 
 /**
@@ -8,22 +10,27 @@
  */
 char *leet(char *s1)
 {
-	int i = -1, n = 0;
+	int i, u = -1, n = 0;
+	char leetA[] = "aeotl";
+	char leetAA[] = "AEOTL";
+	char leetV[] = "43071";
+	char *ptr = NULL;
+	char *ptr2 = NULL;
 
-	while (i != '\0')
+	for (i = 0; i < 5; i++)
 	{
-		i = (int)s1[n];
-		if (s1[n] == 'a' || s1[n] == 'A')
-			s1[n] = '4';
-		if (s1[n] == 'e' || s1[n] == 'E')
-			s1[n] = '3';
-		if (s1[n] == 'o' || s1[n] == 'O')
-			s1[n] = '0';
-		if (s1[n] == 't' || s1[n] == 'T')
-			s1[n] = '7';
-		if (s1[n] == 'l' || s1[n] == 'L')
-			s1[n] = '1';
-		n++;
+		u = -1;
+		while (u != '\0')
+		{
+			u = s1[n];
+			ptr = strchr(s1, leetA[i]);
+			ptr2 = strchr(s1, leetAA[i]);
+			if (ptr != NULL)
+				*ptr = leetV[i];
+			else if (ptr2 != NULL)
+				*ptr2 = leetV[i];
+			n++;
+		}
 	}
 	return (s1);
 }
