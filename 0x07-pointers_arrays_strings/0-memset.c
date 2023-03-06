c671dc3291722b02dafa29756fa082d30300bf1b
0xSakone <abdramanesakone4@gmail.com>
2023-03-06 21:29:11 +0000

    Memset array filler

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memset - check the code
+ * @s: string
+ * @b: character
+ * @n: number of bit
+ * Return: pointer to char.
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	return memset(s, b, n);
+}
