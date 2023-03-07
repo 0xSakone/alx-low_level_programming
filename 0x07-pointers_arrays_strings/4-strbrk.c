f24323d26131c33050917fbbf3ac4e9f2ad004f9
0xSakone <abdramanesakone4@gmail.com>
2023-03-07 01:31:48 +0000

    strbrk any occurence

---
+++ 0x07-pointers_arrays_strings/4-strbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - check the code
+ * @s: string
+ * @accept: character
+ *
+ * Return: pointer to char.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
