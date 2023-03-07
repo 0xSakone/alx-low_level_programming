c477944557fe343cdab8e97ec8c67ced0a19c1aa
0xSakone <abdramanesakone4@gmail.com>
2023-03-07 01:20:56 +0000

    Strchr searcher

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - check the code
+ * @s: string
+ * @c: character
+ * Return: pointer to char.
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
