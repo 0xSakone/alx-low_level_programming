eb71512794741e966b12ec93d034ac40f634e709
0xSakone <abdramanesakone4@gmail.com>
2023-03-07 02:29:02 +0000

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * set_string - set string
+ * @s: string
+ * @to: string
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
