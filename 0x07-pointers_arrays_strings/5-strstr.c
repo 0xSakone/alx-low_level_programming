8bb226364f1cdb7a6ba403b524387aff21c612d6
0xSakone <abdramanesakone4@gmail.com>
2023-03-07 01:35:55 +0000

    strstr any occurence

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - check the code
+ * @haystack: string
+ * @needle: string
+ *
+ * Return: pointer to char.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
