b053fe35ddafe51725c9501f33a8268cdb3373ee
0xSakone <abdramanesakone4@gmail.com>
2023-03-08 05:13:42 +0000

    Print string reversed with recusion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * _print_rev_recursion - check the code
+ * @s: string
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	char *ss = (char *) malloc((strlen(s) - 1) * sizeof(char));
+
+	if (strlen(s) - 1 == 0)
+	{
+		return;
+	}
+	_putchar(s[strlen(s) - 1]);
+	_print_rev_recursion(strncpy(ss, s, strlen(s) - 1));
+}
+
