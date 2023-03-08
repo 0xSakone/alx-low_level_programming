0258026db27c6d12dcee2a2808c8794e2fa6bd92
0xSakone <abdramanesakone4@gmail.com>
2023-03-08 22:43:00 +0000

    Pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion - check the code
+ * @x: integer
+ * @y: integer
+ *
+ * Return: factorial
+ */
+int _pow_recursion(int x, int y)
+{
+	int i;
+
+	i = pow(x, y);
+	if (i <= 0)
+	{
+		return (-1);
+	}
+	return (pow(x, y));
+}
