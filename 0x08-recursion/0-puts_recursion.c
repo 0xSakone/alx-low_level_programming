14c54560914ce6befe670509a82f6fbd000499b6
0xSakone <abdramanesakone4@gmail.com>
2023-03-08 03:48:22 +0000

    0 Puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts_recursion - check the code
+ * @s: string
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+
