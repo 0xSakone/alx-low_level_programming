0080a3efb4833d4ba33708068998babe4e0c9d32
0xSakone <abdramanesakone4@gmail.com>
2023-03-10 20:13:43 +0200

    Static libraries

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+int _isupper(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
+int _isdigit(int c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strchr(char *s, char b);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+int _islower(char c);
+void _puts(char *s);
+int _strcmp(char *s1, char *s2);
+unsigned int _strspn(char *s, char *accept);
+int _isalpha(char c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _abs(int n);
+char *_strcpy(char *s1, char *s2);
+int _putchar(char a);
+
+#endif
