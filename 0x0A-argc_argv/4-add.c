c5ea3f7cd1c12047536ef65a8a797aa0986d7e49
0xSakone <abdramanesakone4@gmail.com>
2023-03-10 21:11:18 +0200

    add positive number

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * display_number - print number
+ * @n: integer
+ * Return: nothing
+ */
+void display_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		display_number(n / 10);
+		n %= 10;
+	}
+	_putchar(n + '0');
+}
+
+/**
+ * main - entry function
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0 as successs
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i, u, si;
+	char *s, *ss = "Error";
+
+	for (i = 1; i < argc; i++)
+	{
+		u = 0;
+		s = argv[i];
+		while (s[u] != '\0')
+		{
+			if (s[u] >= '0' && s[u] <= '9')
+				sum += atoi((s + u));
+			else
+			{
+				si = 0;
+				while (ss[si] != '\0')
+					_putchar(ss[si++]);
+				return (0);
+			}
+			u++;
+		}
+	}
+	display_number(sum);
+	_putchar('\n');
+	return (0);
+}
