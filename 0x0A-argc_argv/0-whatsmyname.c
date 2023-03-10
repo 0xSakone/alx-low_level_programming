91134ec2fd9c13e9b219e7d8c12e10a508abcc78
0xSakone <abdramanesakone4@gmail.com>
2023-03-10 20:33:09 +0200

    show script name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry function
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0 as successs
+ */
+int main(int argc, char *argv[])
+{
+	_puts(argv[argc - 1]);
+	return (0);
+}
