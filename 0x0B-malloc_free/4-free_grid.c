5757e7816d2c7916e552c793cb9426488146779e
0xSakone <abdramanesakone4@gmail.com>
2023-03-15 02:11:54 +0200

    Malloc 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_grid - entry function
+ * @grid: grid array
+ * @height: integer
+ * Return: noting
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	i = 0;
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+}
