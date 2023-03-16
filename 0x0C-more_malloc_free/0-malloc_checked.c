b088dbcfbc27e42dcb492fd85424d0175590d57a
0xSakone <abdramanesakone4@gmail.com>
2023-03-17 01:00:19 +0200

    More Maloc 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - entry function
+ * @b: int
+ * Return: character
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *c;
+
+	c = malloc(b);
+	if (c != NULL)
+	{
+		exit(98);
+	}
+	return (c);
+}
