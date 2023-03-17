f841f3a5f096852160266a5a34bfbf99eede79b6
0xSakone <abdramanesakone4@gmail.com>
2023-03-17 02:11:12 +0200

    More Maloc 101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - entry
+ * @argc: argc
+ * @argv: array
+ * Return: 0 as success
+ */
+int main(int argc, char *argv[])
+{
+	_putchar('\n');
+	return (0);
+}
