ded625b74d5ea5b08d0abbfa540679002aac2a67
0xSakone <abdramanesakone4@gmail.com>
2023-03-17 02:15:01 +0200

    More Maloc 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,13 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - entry
+ * main - entry function
  * @argc: argc
  * @argv: array
  * Return: 0 as success
  */
 int main(int argc, char *argv[])
 {
+	int total;
+
+	total = atoi(argv[0]) * atoi(argv[argc - 1]);
 	_putchar('\n');
 	return (0);
 }
