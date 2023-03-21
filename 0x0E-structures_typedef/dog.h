d9df5947a529dc51535a0db6011660c27685b968
0xSakone <abdramanesakone4@gmail.com>
2023-03-21 03:17:10 +0200

    Structures 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_HEADER_FILE
+#define DOG_HEADER_FILE
+
+/**
+ * struct dog - Dog type.
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
