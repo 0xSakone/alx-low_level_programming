a44c90a178b88ebb32d5d0c97294099328c7d835
0xSakone <abdramanesakone4@gmail.com>
2023-03-21 03:22:32 +0200

    Structures 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - entry function
+ * @d: structure pointer
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
