d2728796a1115cff781502f02ab944cc2e6854dd
0xSakone <abdramanesakone4@gmail.com>
2023-03-23 01:30:22 +0200

    Function pointer 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_name - display name
+ * @name: string name
+ * @f: function to call to display
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
