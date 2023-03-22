3cdf9cafc2eaccca4749998824c54fb404f4f21e
0xSakone <abdramanesakone4@gmail.com>
2023-03-23 01:41:51 +0200

    Function pointer 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - call action on all other parameter array item
+ * @array: int array
+ * @size: array size
+ * @action: function to call
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		action(array[i]);
+	}
+}
