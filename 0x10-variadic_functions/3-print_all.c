b74b8645d017b67a0c85da9448420e9dae23980d
0xSakone <abdramanesakone4@gmail.com>
2023-03-24 02:16:42 +0200

    Variadic 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - strings
+ * @format: separator character
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	printf("\n");
+}
