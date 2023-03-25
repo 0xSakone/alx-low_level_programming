e19ba3a11dc2e047f0686b70c2ab4491e4623b62
ssakone <abdramanesakone1@gmail.com>
2023-03-26 00:25:52 +0200

    ✨ Add String & Character formatting

--- _printf.c
+++ _printf.c
@@ -1,6 +1,67 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * cprintf - display character to standart output
+ * @buf: string buffer
+ * @len: lenght of given format text
+ * Return: number of caracter;
+ */
+int cprintf(char *buf, int len)
+{
+	return (write(1, buf, len));
+}
+
+/**
+ * print_number - print number
+ * @n: number to print
+ * @count: number character count
+ * Return: number of caracter
+ */
+int print_number(int n, int count)
+{
+	int n2;
+	char *sign = "-";
+	char c[1];
+
+	if (n < 0)
+	{
+		n2 = -n;
+		cprintf(sign, 1);
+	}
+	else
+		n2 = n;
+
+	if ((n2 % 10) != n2)
+		count = print_number(n2 / 10, count++);
+
+	c[0] = (char)((n2 % 10) + '0');
+	cprintf(c, 1);
+	return (count);
+}
+
+/**
+ * character_format - character formater
+ * @c: character
+ * Return: number of character
+ */
+int character_format(char c)
+{
+	return (cprintf(&c, 1));
+}
+
+/**
+ * string_format - string text formater
+ * @s: string
+ * Return: number of character
+ */
+int string_format(char *s)
+{
+	cprintf(s, _strlen(s));
+	return (_strlen(s) - 1);
+}
 
 /**
  * _printf - printf function simulator
@@ -9,33 +70,44 @@
  */
 int _printf(const char * const format, ...)
 {
-	int i = 0;
-	char *str;
+	int i = 0, count = 0;
+	char current_character;
 	va_list args;
 
 	va_start(args, format);
 	while (format && format[i])
 	{
-		switch (format[i])
+		current_character = format[i];
+		if (current_character == '%')
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				printf("%s", str ? str : "(nil)");
-				break;
-			default:
-				i++;
-				continue;
+			i++;
+			current_character = format[i];
+			switch (current_character)
+			{
+				case 'c':
+					count++;
+					character_format((char)va_arg(args, int));
+					break;
+				case 'i':
+					count += print_number(va_arg(args, int), 1);
+					break;
+				case 'f':
+					count += print_number(va_arg(args, int), 1);
+					break;
+				case 'd':
+					count += print_number(va_arg(args, int), 1);
+					break;
+				case 's':
+					count += string_format(va_arg(args, char *));
+					break;
+			}
+		}
+		else
+		{
+			count++;
+			cprintf(&current_character, 1);
 		}
 		i++;
 	}
-	return (0);
+	return (count);
 }
--- test/main.c
+++ test/main.c
@@ -10,18 +10,22 @@
 int main(void)
 {
     int len;
-    int len2;
+    //int len2;
     // unsigned int ui;
     // void *addr;
+    char *s = "Abdramane Sakone";
 
     len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
+    // len2 = printf("Let's try to printf a simple sentence.\n");
     // ui = (unsigned int)INT_MAX + 1024;
     // addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Character Test:[%c, %c]\n", 'A', 'B');
+    len = _printf("Your name:[%s]\n", s);
+    // printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Last length:[%d]\n", len);
     _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
+    // printf("Negative:[%d]\n", -762534);
     // _printf("Unsigned:[%u]\n", ui);
     // printf("Unsigned:[%u]\n", ui);
     // _printf("Unsigned octal:[%o]\n", ui);
