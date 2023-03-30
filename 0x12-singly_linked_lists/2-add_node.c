25d70524381dbfce36959e3e8b1a940e484ac525
0xSakone <abdramanesakone4@gmail.com>
2023-03-31 00:15:39 +0200

    ✨ linkded list 2.1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 
 /**
+ * _strlen - finds and outputs length of string
+ * @s: pointer to string.
+ *
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+
+/**
  * add_node - add a node at top
  * Description: no description
  * @head: previous node
