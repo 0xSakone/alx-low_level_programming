e1041d4d1b8d08202d6b920d1b8a4bc689b49a99
0xSakone <abdramanesakone4@gmail.com>
2023-03-31 00:10:52 +0200

    ✨ linkded list 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - add a node at top
+ * Description: no description
+ * @head: previous node
+ * @str: strings
+ * Return: number node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node = malloc(sizeof(list_t));
+
+	node->str = (char *)str;
+	node->len = _strlen((char *)str);
+	if (head == NULL)
+		*head = node;
+	else
+	{
+		node->next = *head;
+		*head = node;
+	}
+
+	if (node->str == NULL)
+	{
+		node->str = "(nil)";
+		node->len = 0;
+	}
+	return (node);
+}
