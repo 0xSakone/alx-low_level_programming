22c51c6e5f126bc7e08d30e6e8a070d39508423c
0xSakone <abdramanesakone4@gmail.com>
2023-03-31 02:36:40 +0200

    ✨ linkded list 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - print all print list
+ * Description: no description
+ * @head: head node
+ * Return: number node
+ */
+void free_list(list_t *head)
+{
+	list_t *next = NULL;
+
+	while ((next = head) != NULL)
+	{
+		head = head->next;
+		free(next->str);
+		free(next);
+	}
+}
