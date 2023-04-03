f657928807c53d40db0a868e9675afea42a2cd7c
0xSakone <abdramanesakone4@gmail.com>
2023-04-03 17:23:15 +0200

    ✨ More linkded list 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - print all print list
+ * @head: head node
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+	listint_t *node = (listint_t *) *head;
+
+	while (node)
+	{
+		next = node;
+		node = node->next;
+		free(next);
+	}
+	free(node);
+}
