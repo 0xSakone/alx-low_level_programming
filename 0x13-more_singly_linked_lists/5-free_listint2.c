84032b995b08e04f31cc45462f7a567c460fe446
0xSakone <abdramanesakone4@gmail.com>
2023-04-03 23:58:21 +0200

    ✨ More linkded list 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,7 +14,11 @@ void free_listint2(listint_t **head)
 	listint_t *node = (listint_t *) *head;
 
 	if (head == NULL)
+	{
+		free(next);
+		free(node);
 		return;
+	}
 	while (node)
 	{
 		next = node;
