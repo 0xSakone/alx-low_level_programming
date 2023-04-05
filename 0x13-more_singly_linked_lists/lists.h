17eae78b7c5f3c60608d035e2341379ee4b3ea71
0xSakone <abdramanesakone4@gmail.com>
2023-04-05 03:04:08 +0200

    ✨ More linkded list 101

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,21 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listp_s - second linked list
+ * @p: pointer
+ * @next:  next node
+ * Description: another pointer
+ *
+ */
+
+typedef struct listp_s
+{
+    void *p;
+    struct listp_s *next;
+
+} listp_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
