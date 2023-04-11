e796dac8c595e6b4379e33a6f80f0b48a682f6dc
0xSakone <abdramanesakone4@gmail.com>
2023-04-11 04:28:36 +0200

    0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
