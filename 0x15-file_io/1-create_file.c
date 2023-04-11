9c0b618733b7dad79e2ef00e198da04cd83ac0d6
0xSakone <abdramanesakone4@gmail.com>
2023-04-11 03:49:55 +0200

    1.3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,13 +31,14 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
 	content_count = _strlen(text_content);
-	if (content_count == 0 || text_content == NULL)
-		write(file, "", 0);
-	else
+	if (write(file, text_content, content_count) == -1)
 	{
-		if (write(file, text_content, content_count) == -1)
-			return (-1);
+		close(file);
+		return (-1);
 	}
 	close(file);
 	return (1);
