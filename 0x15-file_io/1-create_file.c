16f8615da3e80736cf9d144cfc23e4d6c35ffd84
0xSakone <abdramanesakone4@gmail.com>
2023-04-11 03:39:12 +0200

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlen - count str len
+ * @s: strings
+ * Return: count
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
+}
+
+/**
+ * create_file - function to create file with content
+ * @filename: the name of file to create
+ * @text_content: the content of file should be create
+ * Return: result
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int content_count;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	if (file == -1)
+		return (-1);
+
+	content_count = _strlen(text_content);
+	if (content_count == 0)
+	{
+		close(file);
+		return (1);
+	}
+
+	if (write(file, text_content, content_count) == -1)
+		return (-1);
+	return (1);
+}
